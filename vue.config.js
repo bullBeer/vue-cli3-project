bf9d74bc6fe19a4132ad05a9a14d84a7bd5ec44a
bullBear <479514106@qq.com>
2019-09-15 18:22:08 +0800

    移除css extract

--- vue.config.js
+++ vue.config.js
@@ -7,9 +7,6 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   outputDir: process.env.BASE_URL,
   assetsDir: 'static',
-  css: {
-    extract: true
-  },
   configureWebpack: {
     performance: {
       hints: false
